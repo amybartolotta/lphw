85a47a24760b9b3416e6bb0e6fab2f59a9795ea6
Amy <amyleighbartolotta@gmail.com>
2022-06-03 15:26:23 -0400

    changed exercise 23

--- ex23.py
+++ ex23.py
@@ -3,11 +3,16 @@ script, input_encoding, error = sys.argv
 
 
 def main(language_file, encoding, errors):
+	print("we've called main")
+
 	line = language_file.readline()
 	
 	if line:
 		print_line(line, encoding, errors)
+		print("we're about to call main again")
 		return main(language_file, encoding, errors)
+	else:
+		print("no line found, we're not calling main again")
 		
 
 def print_line(line, encoding, errors):
