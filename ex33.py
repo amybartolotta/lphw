56821373cf5726371fd864e871aac4184a904b88
Amy <amyleighbartolotta@gmail.com>
2022-06-10 20:09:21 -0400

    create exercise 33

---
+++ ex33.py
@@ -0,0 +1,20 @@
+
+def buildList(num):
+	i = 0
+	numbers = []
+	for i in range (0, 10):
+		print(f"At the top i is {i}")
+		numbers.append(i)
+	
+	
+		print("Numbers now: ", numbers)
+		print(f"At the bottom i is {i}")
+
+
+
+	print("The numbers: ")
+
+	for num in numbers:
+		print(num)
+		
+buildList(10)
